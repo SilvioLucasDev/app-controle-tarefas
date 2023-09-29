3047ed2efc30f0630e7519a385cb70456214245e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-29 01:18:54 -0300

    refactor: adjust email of reset password

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TarefaController;
 use App\Mail\MensagemTesteMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +29,7 @@ Route::resource('tarefa', TarefaController::class);
 
 Route::get('mensagem-teste', function() {
     return new MensagemTesteMail();
+
+    // Mail::to('silviolucas_santos@hotmail.com')->send(new MensagemTesteMail);
+    // return 'E-mail enviado com sucesso!';
 });
