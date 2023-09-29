1d7828a9f29b3e6335ed0d2ce5b93de40a6db5b3
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-29 13:44:11 -0300

    feat: implement methods edit and update in TarefaController

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -20,7 +20,7 @@ class TarefaController extends Controller
     public function index()
     {
         $user_id = Auth::user()->id;
-        $tarefas = Tarefa::where('user_id', $user_id)->paginate(1);
+        $tarefas = Tarefa::where('user_id', $user_id)->paginate(10);
         return view('tarefa.index', ['tarefas' => $tarefas]);
     }
 
@@ -58,7 +58,8 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        //
+        if ($tarefa->user_id !== Auth::user()->id) return view('acesso-negado');
+        return view('tarefa.edit', ['tarefa' => $tarefa]);
     }
 
     /**
@@ -66,7 +67,9 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        //
+        if ($tarefa->user_id !== Auth::user()->id) return view('acesso-negado');
+        $tarefa->update($request->all());
+        return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
 
     /**
