87ef22c59b8b97496714f91274fdd65739a58686
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-29 12:41:26 -0300

    refactor: link user in tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -38,7 +38,9 @@ class TarefaController extends Controller
      */
     public function store(Request $request)
     {
-        $tarefa = Tarefa::create($request->all());
+        $dados = $request->all();
+        $dados['user_id'] = Auth::user()->id;
+        $tarefa = Tarefa::create($dados);
         $destinatario = Auth::user()->email;
         Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
