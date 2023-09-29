001aa535422db834bd3aeedc7bd66617631345c7
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-29 01:28:19 -0300

    feat: apply middleware of verify email

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('verified');
 
-Route::resource('tarefa', TarefaController::class);
+Route::resource('tarefa', TarefaController::class)->middleware('verified');
 
 Route::get('mensagem-teste', function() {
     return new MensagemTesteMail();
